034bb9de855fb6a75e29e362da829c1ae44c8d0f
diliupg <unitygamecodersl@gmail.com>
2022-08-26 23:02:20 +0530

    L191 ending the game

--- Source/SimpleShooter/ShooterCharacter.cpp
+++ Source/SimpleShooter/ShooterCharacter.cpp
@@ -4,6 +4,7 @@
 #include "ShooterCharacter.h"
 #include "Gun.h"
 #include "Components/CapsuleComponent.h"
+#include "SimpleShooterGameModeBase.h"
 
 // Sets default values
 AShooterCharacter::AShooterCharacter():
@@ -65,6 +66,11 @@ float AShooterCharacter::TakeDamage( float DamageAmount, struct FDamageEvent con
 	{
 		DetachFromControllerPendingDestroy( );
 		GetCapsuleComponent( )->SetCollisionEnabled( ECollisionEnabled::NoCollision );
+		ASimpleShooterGameModeBase* GameMode = GetWorld( )->GetAuthGameMode<ASimpleShooterGameModeBase>( );
+		if ( GameMode != nullptr )
+		{
+			GameMode->PawnKilled( this );
+		}
 	}
 	return DamageToApply;
 }
